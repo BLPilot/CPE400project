5343f1bfcdc458b6d77688a0cd1c79cf801515a3
Ubuntu <Jose Martin>
2021-04-20 02:13:56 -0400

    Main function added (Doesnt contain much)

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/*
+Concurrent File Transfer Algorithm
+
+
+
+*/
+#include <iostream>
+
+
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+	if (argc != 2 && argc != 3) {
+	
+		printf("ERROR: Folder not found. Please provide file.\n");
+		exit(EXIT_FAILURE);
+		
+		
+	}
+	
+	char * fp = argv[2];
+	
+	
+	return 0;
+
+
+}
