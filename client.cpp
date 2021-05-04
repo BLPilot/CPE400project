668af162c9d22d43809ae2eb8eea36709e6db455
Ubuntu <Jose>
2021-05-04 05:38:17 -0400

    client asks for file and server sends it, client creates new file

--- client.cpp
+++ client.cpp
@@ -1,52 +1,125 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+// client code for UDP socket programming
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <iostream>
+#include <fstream>
+  
+#define IP_PROTOCOL 0
+#define IP_ADDRESS "127.0.0.1" // localhost
+#define PORT_NO 15050
+#define NET_BUF_SIZE 32
+#define cipherKey 2
+#define sendrecvflag 0
 
 using namespace std;
   
-#define PORT     8080
-#define MAXLINE 1024
+void clear(char* b);
+char Decrypted(char ch);
+int receiveFile(char* buf, int s, char * input_name);
   
-// Driver code
-int main() {
+
+  
+// driver code
+int main()
+{
     int sockfd;
-    char buffer[MAXLINE];
-    char hello[] = "Hello from client";
-    struct sockaddr_in     servaddr;
-  
-    // Creating socket file descriptor
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-  
-    memset(&servaddr, 0, sizeof(servaddr));
-      
-    // Filling server information
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(PORT);
-    servaddr.sin_addr.s_addr = INADDR_ANY;
-      
     int n;
-    socklen_t len;
-      
-    sendto(sockfd, (const char *)hello, strlen(hello),
-        MSG_CONFIRM, (const struct sockaddr *) &servaddr, 
-            sizeof(servaddr));
-    printf("Hello message sent.\n");
-          
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
-                MSG_WAITALL, (struct sockaddr *) &servaddr,
-                &len);
-    buffer[n] = '\0';
-    printf("Server : %s\n", buffer);
+    struct sockaddr_in addr_con;
+    socklen_t addrlen = sizeof(addr_con);
+    
+    //filling server information
+    addr_con.sin_family = AF_INET;
+    addr_con.sin_port = htons(PORT_NO);
+    addr_con.sin_addr.s_addr = inet_addr(IP_ADDRESS);
+    char net_buf[NET_BUF_SIZE];
+    FILE* fp;
+    FILE* input;
+    char input_name[100];
+    sprintf(input_name, "Input_file.txt");
   
-    close(sockfd);
+    // creating socket
+    sockfd = socket(AF_INET, SOCK_DGRAM,
+                    IP_PROTOCOL);
+                    
+   if(sockfd <0){
+   		perror("Socket creation failed");
+   		exit(EXIT_FAILURE);
+   }else{
+		cout<<"Socket created succesfully"<<endl;
+   }
+                    
+                    
+    while (1) {
+        cout<<endl<<"Please enter file:"<<endl;
+        cin>>net_buf;
+        sendto(sockfd, net_buf, NET_BUF_SIZE,
+               sendrecvflag, (struct sockaddr*)&addr_con,
+               addrlen);
+  
+       	cout<<endl<<"Data Received: "<<endl;
+  
+        while (1) {
+            // receive
+            clear(net_buf);
+            n = recvfrom(sockfd, net_buf, NET_BUF_SIZE,
+                              sendrecvflag, (struct sockaddr*)&addr_con,
+                              &addrlen);
+                              
+            
+             
+          
+			
+            
+            
+            // process
+            if (receiveFile(net_buf, NET_BUF_SIZE, input_name)) {
+			
+				
+                
+                break;
+            }
+            
+           
+        }
+        cout<<endl<<endl;
+    }
+    return 0;
+}
+
+// function to clear buffer
+void clear(char* b){
+    int i;
+    for (i = 0; i < NET_BUF_SIZE; i++)
+        b[i] = '\0';
+}
+
+// function for decryption of message
+char Decrypted(char ch){
+    return ch - cipherKey;
+}
+
+// function to receive file
+int receiveFile(char* buf, int s, char * input_name){
+    int i;
+    char ch;
+    ofstream out(input_name);
+    
+    for (i = 0; i < s; i++) {
+        ch = buf[i];
+        ch = Decrypted(ch);
+        if (ch == EOF)
+            return 1;
+        else
+            printf("%c", ch);
+            out << ch;
+    }
+    
+    out.close();
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,115 +1,140 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+// server code for UDP socket programming
 #include <arpa/inet.h>
 #include <netinet/in.h>
-#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <iostream>
+  
+#define IP_PROTOCOL 0
+#define PORT_NO 15050
+#define NET_BUF_SIZE 32
+#define cipherKey 2
+#define sendrecvflag 0
+#define nofile "File Not Found!"
 
 using namespace std;
   
-#define PORT     8080
-#define MAXLINE 1024
-#define MAX 80
+void clear(char* b);
+char Encrypt(char ch); 
+int sendFile(FILE* fp, char* buf, int s);
+  
 
-void infoExchange(int sockfd);
-
-// Driver code
-int main() {
+  
+// driver code
+int main()
+{
     int sockfd;
-    char buffer[MAXLINE];
-    char hello[] = "Hello from server";
-    struct sockaddr_in servaddr, cliaddr;
-      
-    // Creating socket file descriptor and verification
-    
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0))< 0) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
-    }else{
-    	cout<<"Server socket created successfully"<<endl;
-    }
-    
-    
-   //possible solutoin to bind: address already in use error
-   //didnt work but could if figured out
-   //so far just restart computer/virtual machine if error encountered
-   /*int tr=1;
-	//kill "Address already in use" error message
-	if (setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&tr,sizeof(int)) == -1) {
-    	perror("setsockopt");
-    	exit(1);
-	}*/
-	 
-    memset(&servaddr, 0, sizeof(servaddr));
-    memset(&cliaddr, 0, sizeof(cliaddr));
-      
-    // Filling server information (assigning IP, Port)
-    servaddr.sin_family    = AF_INET; // IPv4
-    servaddr.sin_addr.s_addr = INADDR_ANY;
-    servaddr.sin_port = htons(PORT);
-      
-    // Bind the socket with the server address
-    if ( bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0 ){
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }else{
-    	cout<<"Server Client Successfullly binded"<<endl;
-    
-    }
-      
     int n;
-    socklen_t len;
-  
-    len = sizeof(cliaddr);  //len is value/resuslt
-  
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
-                MSG_WAITALL, ( struct sockaddr *) &cliaddr,
-                &len);
-    buffer[n] = '\0';
-    printf("Client : %s\n", buffer);
-    sendto(sockfd, (const char *)hello, strlen(hello), 
-        MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
-            len);
-    printf("Hello message sent.\n"); 
+    struct sockaddr_in addr_con;
+    socklen_t addrlen = sizeof(addr_con);
     
+    //filling server information
+    addr_con.sin_family = AF_INET;
+    addr_con.sin_port = htons(PORT_NO);
+    addr_con.sin_addr.s_addr = INADDR_ANY;
+    char net_buf[NET_BUF_SIZE];
+    FILE* fp;
+  
+    // creating socket
+    sockfd = socket(AF_INET, SOCK_DGRAM, IP_PROTOCOL);
+    
+    if(sockfd <0){
+   		perror("Socket creation failed");
+   		exit(EXIT_FAILURE);
+   }else{
+		cout<<"Socket created succesfully"<<endl;
+   }
+    
+  
+    //Binding socket with server address
+    if (bind(sockfd, (struct sockaddr*)&addr_con, sizeof(addr_con)) == 0){
+        cout<<"Server Binded succesfully"<<endl;
+    }else{
+    	cout<<"Server Binding Failed"<<endl;
+    }
+        
+  
+    while (1) {
+        cout<<endl<<"Waiting for file"<<endl;
+  
+        // receive file name
+        clear(net_buf);
+  
+        n = recvfrom(sockfd, net_buf,
+                          NET_BUF_SIZE, sendrecvflag,
+                          (struct sockaddr*)&addr_con, &addrlen);
+  
+        fp = fopen(net_buf, "r");
+        cout<<endl<<"File Name Received:"<<net_buf<<endl;
+        if (fp == NULL)
+            cout<<endl<<"File could not be opened"<<endl;
+        else
+            cout<<endl<<"File opened"<<endl;
+  
+        while (1) {
+  
+            // process
+            if (sendFile(fp, net_buf, NET_BUF_SIZE)) {
+                sendto(sockfd, net_buf, NET_BUF_SIZE,
+                       sendrecvflag, 
+                    (struct sockaddr*)&addr_con, addrlen);
+                break;
+            }
+  
+            // send
+            sendto(sockfd, net_buf, NET_BUF_SIZE,
+                   sendrecvflag,
+                (struct sockaddr*)&addr_con, addrlen);
+            clear(net_buf);
+        }
+        if (fp != NULL)
+            fclose(fp);
+    }
+    return 0;
+}
 
-      
+//////////////
+//functions///
+//////////////
+
+// function to clear buffer
+void clear(char* b){
+    int i;
+    for (i = 0; i < NET_BUF_SIZE; i++)
+        b[i] = '\0';
+}
+
+// function to encrypt the message
+char Encrypt(char ch){
+    return ch + cipherKey;
+}
+
+// function sending file
+int sendFile(FILE* fp, char* buf, int s){
+    int i, len;
+    if (fp == NULL) {
+        strcpy(buf, nofile);
+        len = strlen(nofile);
+        buf[len] = EOF;
+        for (i = 0; i <= len; i++)
+            buf[i] = Encrypt(buf[i]);
+        return 1;
+    }
+  
+    char ch, ch2;
+    for (i = 0; i < s; i++) {
+        ch = fgetc(fp);
+        ch2 = Encrypt(ch);
+        buf[i] = ch2;
+        if (ch == EOF)
+            return 1;
+    }
     return 0;
 }
 
 
-//Tryna figure out how to do TCP medata information exhange.
-//Got this off tutorial and trying to modify to suit our needs
-//Funciton for TCP medata information exhange
-void infoExchange(int sockfd){
-	char buff[MAX];
-    int n;
-    // infinite loop for chat
-    for (;;) {
-        bzero(buff, MAX);
-  
-        // read the message from client and copy it in buffer
-        read(sockfd, buff, sizeof(buff));
-        // print buffer which contains the client contents
-        printf("From client: %s\t To client : ", buff);
-        bzero(buff, MAX);
-        n = 0;
-        // copy server message in the buffer
-        while ((buff[n++] = getchar()) != '\n')
-            ;
-  
-        // and send that buffer to client
-        write(sockfd, buff, sizeof(buff));
-  
-        // if msg contains "Exit" then server exit and chat ended.
-        if (strncmp("exit", buff, 4) == 0) {
-            printf("Server Exit...\n");
-            break;
-        }
-    }
 
-}
